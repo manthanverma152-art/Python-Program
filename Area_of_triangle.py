5aef05baaa4636941d440b2df8c907b891138754
manthan verma <manthanverma152@gmail.com>
2025-09-08 23:30:56 +0530

    initial commit

---
+++ Area_of_triangle.py
@@ -0,0 +1,6 @@
+length=float(input("enter the length of the rectangle: "))
+width=float(input("enter the width of the rectangle: "))
+
+area = length * width
+
+print("the are of the rectangle is :",area)
