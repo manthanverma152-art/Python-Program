8fa0ddbb534c824420265318ee4bd73f6b54d9f5
manthan verma <manthanverma152@gmail.com>
2025-09-08 23:36:46 +0530

    Average of three numbers

---
+++ Average_of_three_numbers.py
@@ -0,0 +1,6 @@
+a=int(input("enter your number: "))
+b=int(input("enter your number: "))
+c=int(input("enter your number: "))
+
+average=(a+b+c)/2
+print("the average of three numbers is: ",average)
